6c416c21a9f341335f2cc924136c370d37982216
MikEKun0 <lavlinskiy2001@mail.ru>
2022-02-20 19:18:26 +0300

    added all tests

--- src/test/java/inc/mimik/SpaceCraftMoveTests.java
+++ src/test/java/inc/mimik/SpaceCraftMoveTests.java
@@ -8,68 +8,157 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("SpaceCraft")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SpaceCraftMoveTests {
     private static Spacecraft vessel;
 
-    @Test
-    @BeforeAll
-    public static void isSuccessfulInit() {
 
-        Star before = new Star("Initial star", new ArrayList<Dock>() {{
+    @BeforeEach
+    public  void Init() {
+        Star before = new Star("Initial star", new ArrayList<>() {{
             add(new Dock("First"));
         }});
         for (Dock e : before.getDocks()) {
             e.setStar(before);
         }
-
-        vessel = new Spacecraft(before);
-        assertEquals(before, vessel.getOrbital());
+        vessel = new Spacecraft(before, null);
     }
 
     @Test
     @Order(1)
-    @DisplayName("Move to star")
+    @DisplayName("Move to star success")
     public void isSuccessfulMoveToStar() {
-
-        Star after = new Star("Final star", new ArrayList<Dock>() {{
+        Star after = new Star("Final star", new ArrayList<>() {{
             add(new Dock("First"));
         }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
         vessel.jumpTo(after);
-        assertEquals(after, vessel.getOrbital());
-
-
+        assertEquals(after, vessel.getOrbital(), "");
     }
 
     @Test
     @Order(2)
-    @DisplayName("Move to free dock")
-    public void isSuccessfulMoveToDoc() {
-        Dock freeDock = new Dock("Free dock");
-        Star after = new Star("Final star", new ArrayList<Dock>() {{
-            add(freeDock);
-        }});
-        vessel.jumpTo(after);
-        assertEquals(after, vessel.getOrbital());
-
-        Stop stop = vessel.stop("Preventive examination");
-        assertNotNull(stop);
-        assertEquals(stop.getDock(), freeDock);
-        assertEquals(stop.getDock().getIsFree(), Boolean.FALSE);
+    @DisplayName("Move to star fail")
+    public void isNotSuccessfulMoveToStar() {
+        Star after = null;
+        assertThrows(NullPointerException.class, () -> {
+            vessel.jumpTo(after);
+        });
     }
 
     @Test
     @Order(3)
-    @DisplayName("Move to busy dock")
-    public void isNotSuccessfulMoveToDoc() {
-        Dock notFreeDock = new Dock("Not free");
-        notFreeDock.setIsFree(Boolean.FALSE);
-        Star after = new Star("Final star", new ArrayList<Dock>() {{
-            add(notFreeDock);
+    @DisplayName("Move to free dock success")
+    public void isSuccessfulMoveToDoc() {
+        Dock freeDock = new Dock("Free dock");
+        Star after = new Star("Final star", new ArrayList<>() {{
+            add(freeDock);
         }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
+
         vessel.jumpTo(after);
         assertEquals(after, vessel.getOrbital());
 
-        Stop stop = vessel.stop("Preventive examination");
-        assertNull(stop);
+        vessel.stop("Preventive examination", 3);
+        Stop stop = vessel.getStop();
+        assertNotNull(stop);
+        assertEquals(stop.getDock(), freeDock);
+        assertEquals(stop.getDock().getIsFree(), Boolean.FALSE);
+
+
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Move to busy dock fail")
+    public void isNotSuccessfulMoveToDoc() {
+        Dock notFreeDock = new Dock("Not free");
+        notFreeDock.setIsFree(Boolean.FALSE);
+        Star after = new Star("Final star", new ArrayList<>() {{
+            add(notFreeDock);
+        }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
+
+        vessel.jumpTo(after);
+        assertEquals(after, vessel.getOrbital());
+
+        vessel.stop("Preventive examination", 3);
+        assertNull(vessel.getStop());
+
+
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Move to dock twice fail")
+    public void moveToDockTwice() {
+        Dock freeDock = new Dock("Free dock");
+        Dock secondFreeDock = new Dock("Second Dock");
+
+        Star after = new Star("Final star", new ArrayList<>() {{
+            add(freeDock);
+            add(secondFreeDock);
+        }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
+
+        vessel.jumpTo(after);
+        assertEquals(after, vessel.getOrbital());
+        vessel.stop("Preventive examination", 3);
+        Stop stop = vessel.getStop();
+        assertNotNull(stop);
+        assertEquals(stop.getDock().getIsFree(), Boolean.FALSE);
+
+        vessel.stop("Preventive examination", 3);
+        assertNotNull(stop);
+        assertEquals(stop, vessel.getStop());
+        assertEquals(freeDock.getIsFree() || secondFreeDock.getIsFree(), Boolean.TRUE);
+
+
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("leave dock success")
+    public void leaveDock() {
+        Dock freeDock = new Dock("Free dock");
+        Star after = new Star("Final star", new ArrayList<>() {{
+            add(freeDock);
+        }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
+        vessel.jumpTo(after);
+        vessel.stop("Preventive examination", 0);
+        Boolean res = vessel.leave();
+        assertEquals(res, Boolean.TRUE);
+
+
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("leave dock fail")
+    public void leaveDockFail() {
+        Dock freeDock = new Dock("Free dock");
+        Star after = new Star("Final star", new ArrayList<>() {{
+            add(freeDock);
+        }});
+        for (Dock e : after.getDocks()) {
+            e.setStar(after);
+        }
+        vessel.jumpTo(after);
+        vessel.stop("Preventive examination", 3);
+        Boolean res = vessel.leave();
+        assertEquals(res, Boolean.FALSE);
+
+
     }
 }
